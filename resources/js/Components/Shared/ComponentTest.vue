d847c559b736487d28653c7a8fbd21e2ef62ccc7
denissabiao <denis.sabiao@gmail.com>
2023-03-11 11:07:07 -0300

    First Commit

---
+++ resources/js/Components/Shared/ComponentTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-row mx-10 mt-10 ">
+        <div class="mx-5 text-center bg-teal-200 rounded-md basis-1/3">
+            <div class="flex items-center justify-center h-full">
+                <p class="text-center">Seu texto aqui</p>
+            </div>
+        </div>
+        <div class="mx-5 text-center rounded-md basis-1/3 bg-violet-400">
+            <div class="flex items-center justify-center h-full">
+                <p class="text-center">Seu texto aqui</p>
+            </div>
+        </div>
+        <slot/>
+        <div class="justify-center mx-5 text-center bg-blue-500 rounded-md basis-1/3 ">
+            <div class="flex items-center justify-center h-full">
+                <p class="text-center">Seu texto aqui</p>
+            </div>
+        </div>
+        
+        <div class="w-full overflow-hidden text-center rounded-md bg-lime-400 lg:h-80 basis-1/3">
+            <div class="flex items-center justify-center h-full">
+                <p class="text-center">Seu texto aqui</p>
+            </div>
+        </div>
+
+    </div>
+</template>
+  
+ 
+
---
+++ resources/js/Pages/helloTail.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ComponentTest from '@/Components/Shared/ComponentTest.vue';
+import Pagination from '@/Components/Shared/Pagination.vue';
+import { createStore } from 'vuex'
+</script>
+  
+<template>
+    <Pagination />
+    <ComponentTest>Your Profile</ComponentTest>
+    <a @click="increment"> Clique </a>
+</template>
+
+<script>
+const store = createStore({
+    state() {
+        return {
+            count: 55
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    },
+    actions: {
+        increment(context) {
+            context.commit('increment')
+        }
+    }
+
+})
+store.commit('increment')
+
+</script>
