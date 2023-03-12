d08337b9f23461044f09c0dc6e60476c918afad3
denissabiao <denis.sabiao@gmail.com>
2023-03-12 14:36:23 -0300

    Define List User

--- resources/js/Pages/User/List.vue
+++ resources/js/Pages/User/List.vue
@@ -1,11 +1,16 @@
 <template>
-    <MenuTop/>
+  <MenuTop />
+
+  <TableList/>
 </template>
+
+
   
 <script setup>
 
 import MenuTop from '@/Components/Shared/MenuTop.vue';
-
+import TableList from '@/Components/Shared/TableList.vue';
+defineProps({
+  users: Array
+});
 </script>
-
-
