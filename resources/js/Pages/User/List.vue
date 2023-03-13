722e427af9637f7dc4292fae329f7a14cb2f270e
denissabiao <denis.sabiao@gmail.com>
2023-03-13 16:45:09 -0300

    Button Actions List User

--- resources/js/Pages/User/List.vue
+++ resources/js/Pages/User/List.vue
@@ -3,7 +3,7 @@
 
   <section class="container px-4 pt-8 mx-auto ">
     <TableList :groupTable=groupTable>
-      <TableHeader />
+      <TableHeader :tableHeader=tableHeader />
       <ButtonTop :buttonSecondary=buttonSecondary />
 
       <template v-slot:tbody>
@@ -25,6 +25,21 @@
                 <h4 class="text-gray-700 dark:text-gray-200">{{ item.email }}</h4>
               </div>
             </td>
+            <td class="flex-row px-4 py-4 text-sm items whitespace-nowrap">
+              <div class="flex flex-row">
+                <Link type="button"
+                  class="px-4 py-2 m-2 text-white transition duration-500 bg-indigo-500 border border-indigo-500 rounded-md select-none ease hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+                  :href="route('user.edit', item.id)">
+                Editar
+                </Link>
+                <Link type="button"
+                  class="px-4 py-2 m-2 text-white transition duration-500 bg-red-500 border border-red-500 rounded-md select-none ease hover:bg-red-600 focus:outline-none focus:shadow-outline"
+                  :href="route('user.delete', item.id)">
+                Excluir
+                </Link>
+              </div>
+            </td>
+
           </tr>
         </tbody>
       </template>
@@ -38,32 +53,37 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    name: 'eqweqw'
+    name: "eqweqw"
   },
   data() {
     return {
       groupTable: [
-        'Id',
-        'Nome',
-        'Email',
-        'Ações',
+        "Id",
+        "Nome",
+        "Email",
+        "Ações",
       ],
-      buttonSecondary:
-      {
-        secondary: true,
-        buttons:
-          [
-            { name: 'Categoria', href: 'dashboard' },
-            { name: 'Variantes', href: 'dashboard' },
-            { name: 'Tags', href: 'dashboard' },
-            { name: 'Imagens', href: 'dashboard' },
-            { name: 'Coleções', href: 'dashboard' },
-          ]
+      buttonSecondary: {
+        secondary: false,
+        buttons: [
+          { name: "Categoria", href: "dashboard" },
+          { name: "Variantes", href: "dashboard" },
+          { name: "Tags", href: "dashboard" },
+          { name: "Imagens", href: "dashboard" },
+          { name: "Coleções", href: "dashboard" },
+        ]
+      },
+      tableHeader: {
+        title: 'Listar Usuários',
+        btnHeader: [
+          { name: 'Cadastrar Usuários', href:'user.store' },
+        ]
       }
-    }
-  }
+    };
+  },
+  components: { Link }
 }
 </script>
   
@@ -73,6 +93,7 @@ import Pagination from '@/Components/Shared/Table/Pagination.vue';
 import TableList from '@/Components/Shared/Table/TableList.vue';
 import ButtonTop from '@/Components/Shared/Table/ButtonTop.vue';
 import TableHeader from '@/Components/Shared/Table/TableHeader.vue';
+import { Link } from '@inertiajs/vue3';
 defineProps({
   users: Array
 });
