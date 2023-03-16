3a02edbaf37420725af6434084b378366864c7c6
denissabiao <denis.sabiao@gmail.com>
2023-03-15 22:06:02 -0300

    Create Input Component

---
+++ resources/js/Components/Shared/Form/InputText.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-span-1 sm:col-span-3">
+        <label :for=field class="block text-sm font-medium leading-6 text-gray-900">{{ labelField }}</label>
+        <input type="password" v-model="form[field]" @focusin="clearError(field)" :name=field :id=field
+            autocomplete="given-name" :class="
+                !form.errors[field]
+                    ? 'input'
+                    : 'inputError'
+            ">
+        <span v-if="form.errors[field]" class="text-red-600">
+            {{ form.errors[field] }}
+        </span>
+    </div>
+</template>
+
+<script setup>
+
+
+const props = defineProps({
+    field: String,
+    form: Object,
+    type: String,
+    labelField: String,
+});
+
+function clearError(fieldName) {
+    props.form.clearErrors(fieldName)
+}
+</script>
+
+<script >
+export default {
+
+
+}
+</script>
+
+
