25df4afc540c8d6ce6b85ddf22c1ae1a4d61ade1
denissabiao <denis.sabiao@gmail.com>
2023-03-16 12:26:59 -0300

    Create Component Action Button List Table

---
+++ resources/js/Components/Shared/Table/ActionButton.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+
+defineProps({
+    buttonAction: Array,
+    itemIdTable: String,
+});
+
+</script>
+
+
+<template>
+    <td class="flex-row px-4 py-4 text-sm items whitespace-nowrap">
+        <div class="flex flex-row">
+
+            <Link v-for="(item, key) in buttonAction.button" :key="item.key" type="button" :class=item.class
+                :href="route(item.href, itemIdTable)">
+            {{ item.name }}
+            </Link>
+
+        </div>
+    </td>
+</template>
+
+
+
