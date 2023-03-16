a52f2ef52751b0f9222f8db906f6f1e275a893eb
denissabiao <denis.sabiao@gmail.com>
2023-03-15 22:06:19 -0300

    Create Header Form Component

---
+++ resources/js/Components/Shared/Form/topHeader.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="sm:flex sm:items-center sm:justify-between">
+            <div class="flex items-center mb-6">
+                <h2 class="text-3xl font-medium text-gray-800 dark:text-white">{{ titleForm }}</h2>
+            </div>
+        </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    titleForm: String,
+});
+
+
+
+</script>
+
