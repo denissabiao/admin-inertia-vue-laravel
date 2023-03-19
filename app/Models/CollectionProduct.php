7e0d22da09265213c491bc482952cafc91ca3360
denissabiao <denis.sabiao@gmail.com>
2023-03-19 14:25:23 -0300

    Create Product and Collection Model

---
+++ app/Models/CollectionProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CollectionProduct extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Collection';
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function product()
+    {
+
+    }
+}
