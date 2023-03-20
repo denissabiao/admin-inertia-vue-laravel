7ef8caad467a92fd1b173ad877d4bb51fd823150
denissabiao <denis.sabiao@gmail.com>
2023-03-20 18:40:54 -0300

    Implement Methods Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,11 +6,14 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
+use function Termwind\render;
+
 class ProductController extends Controller
 {
     private $product;
 
-    public function __construct(Product $product){
+    public function __construct(Product $product)
+    {
         $this->product = $product;
     }
 
@@ -28,13 +31,30 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $message = [];
+
+        $request->validate([
+            'title' => "required",
+            'description' => "required",
+            'type' => "required",
+            'brand' => "required",
+            'category' => "required",
+            'price' => "required",
+            'stock' => "required",
+        ], $message);
+
+        $this->product->fill($request->input());
+        $this->product->save();
+
+        return redirect()->back()->with('success', 'Produto Cadastrado com Sucesso');
+
     }
 
 
     public function show($id)
     {
-        //
+        $product = $this->product->find($id);
+        return Inertia::render('Product/Edit', ['product' => $product]);
     }
 
 
@@ -44,14 +64,30 @@ class ProductController extends Controller
     }
 
 
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $message = [];
+
+        $request->validate([
+            'title' => "required",
+            'description' => "required",
+            'type' => "required",
+            'brand' => "required",
+            'category' => "required",
+            'price' => "required",
+            'sale' => "required",
+            'stock' => "required",
+        ], $message);
+
+        $this->product->find($request->input('id'))->update($request->except('id'));
+        return redirect()->back()->with('success', 'Produto alterado com sucesso.');
     }
 
 
     public function destroy($id)
     {
-        //
+        $this->product->destroy($id);
+
+        return redirect()->back()->with('success', 'Produto excluido com sucesso.');
     }
 }
